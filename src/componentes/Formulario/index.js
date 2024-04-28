4fd7d512ad73f1503e2a5436053fcbc88148da17
bruno loureiro <brunoloureiro9@yahoo.com.br>
2024-04-28 13:18:46 -0300

    botão de exibição de tira-dúvidas

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -22,13 +22,13 @@ const Formulario = () => {
     <section className='formulario'>
       {!mostrarQuestionario ? (
         <form className="text-center pt-2" onSubmit={aoIniciar}>
-          <Tooltip title={descricao_formulario} position="top" trigger="mouseenter">
           <div className='formulario-btn-informacao'>
-            <span className='btn btn-danger btn-sm'>
-              <i className="fa-solid fa-circle-question"></i> Leia-me 
+          <Tooltip title={descricao_formulario} position="left" trigger="mouseenter">
+            <span className='btn btn-outline-danger border-0'>
+              <i className="fa-solid fa-circle-question fa-xl"></i> Informação
             </span>
-          </div>
           </Tooltip>
+          </div>
           <h1 className='formulario-titulo'>Pesquisa de Avaliação do Serviço da GETIC</h1>
           <h2 className='formulario-subtitulo pb-5 fs-5'>Por favor, avalie cada item a seguir, selecionando a opção que melhor descreve sua experiência.</h2>
           <Tooltip title="Clique para iniciar pesquisa" position="right" trigger="mouseenter">

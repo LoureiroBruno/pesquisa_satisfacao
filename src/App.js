cf0565e8d36790d19027dbab02becfe5e142a067
bruno loureiro <brunoloureiro9@yahoo.com.br>
2024-04-28 13:55:36 -0300

    inserido barra de navegação com as cores padrões

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import NavCores from './componentes/NavCores';
 
 function App() {
   return (
     <div className="App">
       <Banner /> 
+      <NavCores />
       <Formulario />
     </div>
   );
---
+++ src/componentes/NavCores/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavCores.css'; 
+
+const NavCores = () => {
+  return (
+        <div className="linhaColorida"></div>
+    );
+}
+
+export default NavCores;
